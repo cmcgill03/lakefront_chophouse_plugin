545642622ab0ef596607dc0468ffa0e71e0f9028
cmcgill03 <connor.mcgillivray@hotmail.com>
2016-03-24 18:24:05 -0400

    adding widget

--- inc/lakefront_widget.php
+++ inc/lakefront_widget.php
@@ -8,3 +8,13 @@
  * Version: 1.0
 */
 
+class lakefront_testimonial extends WP_Widget {
+	public function __construct() {
+		$lakefront_widget = array(
+		'classname' => 'widget_testimonial',
+		'description' => __( 'Place a certain number of testimonials.') );
+		parent::__construct('testimonial', __('Testimonials', 'lakefront_chophouse'), $lakefront_widget);
+	} 
+}
+
+add_action( 'widgets_init', function(){ register_widget('lakefront_testimonial' ); }); 
