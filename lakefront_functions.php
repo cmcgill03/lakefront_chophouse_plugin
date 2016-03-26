839ad742f88b204ccfe0800cf8a45dd0cfb5a0e4
cmcgill03 <connor.mcgillivray@hotmail.com>
2016-03-26 16:46:34 -0400

    changed shortcode to show post type

--- lakefront_functions.php
+++ lakefront_functions.php
@@ -8,35 +8,39 @@
  * Version: 1.0
  */
  
+ 
  //enqueues the style sheet if needed
 function lakefront_plguin(){ 
 	wp_enqueue_style( 'lakefront_plugin_style', plugins_url('/lakefront_plugin_style.css', __FILE__) );
 }
 add_action('wp_enqueue_scripts','lakefront_plugin');
- 
- 
+
+
+
+
+
+
 function testimonials($atts, $content = null) { //declare the function for the shortcode
         extract(shortcode_atts(array(
                 "num" => '1', //how many posts will be shown
-                "cat" => 'testimonials' //which category the posts will be shown from
+                "posttype" => 'testimonials' //which category the posts will be shown from
         ), $atts));
 		
         global $post; //declares a global variable for post
 		
-        $myposts = get_posts('numberposts='.$num.'&orderby=rand&category_name='.$cat); //gets the posts based on the attributes given and in a random order
+        $myposts = get_posts('numberposts='.$num.'&orderby=rand&post_type='.$posttype); //gets the posts based on the attributes given and in a random order
         foreach($myposts as $post) : //sets up the for loop if it is looking for more than one post
                 setup_postdata($post);
              $testimonial= the_title().'</br>' . the_content(); //determines what information will be displayed
         endforeach;
         return $testimonial; //displays the title and the content
 }
-add_shortcode("testimonials", "testimonials");
- 
+add_shortcode('testimonials', 'testimonials'); 
  
 
 
 /*Call the widget */
 
-require get_plugins() . '/lakefront_widget.php'; 
+//require plugins_url( 'C:/MAMP/htdocs/Wordpress1/wp-content/plugins/lakefront_chophouse_plugin/lakefront_widget.php', dirname(__FILE__) ); 
 
 ?>
